76050236f504c4ba7a922215ab9a4a5e6bba24d5
Gilles Pirio <g36130@gmail.com>
2015-08-20 11:25:20 -0700

    Adding correct websocket dependency

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A Python package for data analysis of the Ripple peer-to-peer network
 
-PyRipple 0.0.1 is a very early version and is subject to change.
+PyRipple 0.0.2 is a very early version and is subject to change.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyripple',
-      version='0.0.1',
+      version='0.0.2',
       description='Python package for data analysis of the Ripple peer-to-peer network',
       url='http://github.com/gip/pyripple',
       author='Gilles Pirio',
@@ -9,4 +9,4 @@ setup(name='pyripple',
       license='Apache License 2.0',
       packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
       zip_safe=False,
-      install_requires= ['numpy', 'matplotlib', 'mpmath', 'pandas', 'websocket'])
+      install_requires= ['numpy', 'matplotlib', 'mpmath', 'pandas', 'websocket-client'])
